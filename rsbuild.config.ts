e3a850a444fd04ae5e00cb7fb0a9dbd2c94b3347
JuanDevEngineer <restrepojuanjose8@gmail.com>
2026-04-19 18:46:27 -0500

    add netlify config

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -4,11 +4,39 @@ import { pluginReact } from '@rsbuild/plugin-react';
 export default defineConfig({
   plugins: [pluginReact()],
   html: {
-    title: 'Personal Portfolio',
+    title: 'Juan Jose Cuadros | Portfolio',
+    meta: {
+      description: 'Personal portfolio of Juan Jose Cuadros - Web Developer',
+      viewport: 'width=device-width, initial-scale=1.0',
+    },
+    tags: [
+      {
+        tag: 'link',
+        attrs: {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com',
+        },
+      },
+      {
+        tag: 'link',
+        attrs: {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: 'anonymous',
+        },
+      },
+      {
+        tag: 'link',
+        attrs: {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap',
+        },
+      },
+    ],
   },
   server: {
     publicDir: {
       name: 'public',
-    }
-  }
+    },
+  },
 });
